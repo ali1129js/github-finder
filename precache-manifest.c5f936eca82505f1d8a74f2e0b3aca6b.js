734c83705e02a3a3eb49197ef4cfdc11dfe4bdb1
ali1129js <ali1129js@gmail.com>
2019-10-17 17:52:44 +0200

    Updates

---
+++ precache-manifest.c5f936eca82505f1d8a74f2e0b3aca6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04135223ec99aa63a9677cd899a6034",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "154e6f050db9ba25224b",
+    "url": "/github-finder/static/css/main.1cd402a0.chunk.css"
+  },
+  {
+    "revision": "77a7857be26509e4c395",
+    "url": "/github-finder/static/js/2.1f4b1e95.chunk.js"
+  },
+  {
+    "revision": "154e6f050db9ba25224b",
+    "url": "/github-finder/static/js/main.37c156cd.chunk.js"
+  },
+  {
+    "revision": "f965038ba67c23ac67b9",
+    "url": "/github-finder/static/js/runtime-main.9505bc40.js"
+  }
+]);
